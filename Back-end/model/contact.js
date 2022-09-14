866c228259c86873b6197ca7537a7e9f3b77771e
Rajesh Manoharan <94797806+RajeshManoharan92@users.noreply.github.com>
2022-09-14 10:30:16 +0530

    Delete Back-end directory

--- Back-end/model/contact.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const contactSchema = new mongoose.Schema({
-    customername: { type: String, default: null },
-    customercontactno: { type: String, default: null },
-    productname: { type: String },
-});
-
-module.exports = mongoose.model("productrentalcontact", contactSchema);
